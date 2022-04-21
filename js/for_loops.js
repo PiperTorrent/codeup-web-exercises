b38df0b6412f076952813f7674d6be79c532ff1e
Piper Torrent <piper.torrent@gmail.com>
2022-04-21 16:16:24 -0500

    loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,26 @@
-
-//1
+//2
 function showMultiplicationTable(num) {
-    for (let x = 1; num *  x <= num * 10; x++) {
-        console.log( num + "x" + x + '=' + (num * x))
+    for (let x = 1; num * x <= num * 10; x++) {
+        console.log(num + "x" + x + '=' + (num * x))
     }
 }
+
 console.log(showMultiplicationTable(7))
+
+
+//3
+for (let x = 10; x < 20; x++) {
+    let random = Math.floor(Math.random() * 180) + 20;
+    if (random % 2 === 0) {
+        console.log(random + " is even")
+    } else {
+        console.log(random + " is odd")
+    }
+}
+
+//4
+for (let x = 1; x < 10; x++) {
+    let str = toString().repeat()
+
+    console.log(str)
+}
