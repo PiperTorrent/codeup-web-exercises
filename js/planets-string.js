e00b42e4659a6d22bcc27c136998c11100a25617
Piper Torrent <piper.torrent@gmail.com>
2022-04-23 21:02:09 -0500

    array

--- js/planets-string.js
+++ js/planets-string.js
@@ -10,18 +10,38 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+ planetsArray = planetsString.split("|")
+     console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
+     */
+
+    document.createElement(<br/>)
+
+
+    /*
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+
+
+   // let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+
+    // <ul>
+    //     <li>Mercury</li>
+    //     <li>Venus</li>
+    //     <li>Earth</li>
+    //     <li>Mars</li>
+    //     <li>Jupiter</li>
+    //     <li>Saturn</li>
+    //     <li>Uranus</li>
+    //     <li>Neptune</li>
+    // </ul>
 
 })();
